b1a107a986939ed1f0892c1537013345032c12d7
PedroAugustoACT <pedro.augusto.dr11@gmail.com>
2022-06-22 10:23:43 -0300

    busca binaria recursiva

--- binary_search/buscabinaria.c
+++ binary_search/buscabinaria.c
@@ -1,31 +1,42 @@
 #include <stdio.h>
 
 
-void busca(int array[], int numeroProcurado, int media){
+int busca(int *array, int numeroProcurado, int inicio, int fim){
 
-    if(numeroProcurado == media){
-        printf("NÚMERO %i ENCONTRADO", numeroProcurado);
-        return;
+    int media;
+    if(inicio <= fim){
+        media = (inicio + fim) / 2;
+        if(numeroProcurado == array[media])
+        {
+            printf("O número %i está no vetor!", numeroProcurado);
+            return 0;
+        }
+        else
+        {
+            if(numeroProcurado < array[media]){
+                return busca(array, numeroProcurado, inicio, media-1);
+            }
+            else if(numeroProcurado > array[media])
+            {
+                return busca(array, numeroProcurado, media+1, fim);
+            }
+        }
     }
-    else if(numeroProcurado > media){
-        return busca(array, numeroProcurado, media++);
+    else{
+         printf("Que pena, o número %i não está no vetor!", numeroProcurado);
+        return -1;
     }
-    else if (numeroProcurado < media) {
-        return busca(array, numeroProcurado, media--);
-    }
-
 }
 
 
 int main(){
 
     int array[10] = {0, 2, 4 , 8, 11, 46, 50, 55, 70, 88}, numeroProcurado;
-    int media = 10/2;
 
     printf("QUAL NÚMERO DESEJA ENCONTRAR? ");
     scanf("%i", &numeroProcurado);
 
-    busca(array, numeroProcurado, media);
+    busca(array, numeroProcurado, 0, 9);
 
     return 0;
 }
